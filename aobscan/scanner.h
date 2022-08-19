729fe90d54bb4ccc3110d1ece0de4ea86f27495a
riari <chanhu0902@gmail.com>
2022-08-19 15:23:36 +0900

    add: 사용할 함수들 정의

---
+++ aobscan/scanner.h
@@ -0,0 +1,17 @@
+#ifndef GUARD
+#define GUARD
+
+#include <vector>
+#include <string>
+#include <Windows.h>
+
+class Scanner {
+public:
+	static std::vector<DWORD> scan(const HANDLE& handle, const std::string& pattern);
+private:
+	static std::vector<DWORD> scan(const HANDLE& handle, const char* pattern, const char* mask);
+	static std::vector<DWORD> scan(const HANDLE& handle, const char* pattern);
+	static std::vector<DWORD> find_pattern(const char* buffer, int buffer_length, const char* pattern, int pattern_length, const char* mask);
+};
+
+#endif
