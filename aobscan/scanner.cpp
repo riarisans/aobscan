afc98644e36361185b1cad0241d53e59d500e95f
riari <chanhu0902@gmail.com>
2022-08-19 17:23:44 +0900

    add: scan기능 추가

--- aobscan/scanner.cpp
+++ aobscan/scanner.cpp
@@ -1,11 +1,30 @@
 #include "scanner.h"
 
+std::vector<std::string> split(std::string str, char delimiter)
+{
+	std::vector<std::string> result;
+	std::string temp;
+	for (int i = 0; !(str[i] == 13 || str[i] == '\n' || str[i] == 0); i++)
+	{
+		if (str[i] == delimiter)
+		{
+			result.push_back(temp);
+			temp.clear();
+
+			continue;
+		}
+		temp.push_back(str[i]);
+	}
+	result.push_back(temp);
+	return result;
+}
+
 std::vector<DWORD> Scanner::find_pattern(const char* buffer, int buffer_length, const char* pattern, int pattern_length, const char* mask) {
 	std::vector<DWORD> result;
-	for (int i = 0; i < buffer_length - pattern_length; ++i) {
+	for (unsigned long i = 0; i < buffer_length - pattern_length; ++i) {
 		bool found = true;
 		for(int j = 0; j < pattern_length; ++j) {
-			if (mask[j] != '?' && buffer[i + j] != pattern[j]) {
+			if (mask[j] != 'x' && pattern[j] != buffer[i+j]) {
 				found = false;
 				break;
 			}
@@ -13,6 +32,44 @@ std::vector<DWORD> Scanner::find_pattern(const char* buffer, int buffer_length, 
 		if (found) {
 			result.push_back(i);
 		}
+	}
+	return result;
+}
+
+std::vector<DWORD> Scanner::scan(const HANDLE& handle, const std::string& pattern) {
+	std::vector<char> mask;
+	std::vector<char> pattern_char;
+	std::vector<std::string> parsed_pattern = split(pattern, ' ');
+	for (int i = 0; i < parsed_pattern.size(); ++i) {
+		if (parsed_pattern[i].find('?') != std::string::npos) {
+			mask.push_back('x');
+			pattern_char.push_back('x');
+		}
+		else { 
+			mask.push_back('.');
+			pattern_char.push_back((char)strtol(parsed_pattern[i].c_str(), NULL, 16));
+		}
+	}
+	return scan(handle, pattern_char.data(), mask.data(), mask.size());
+}
+
+std::vector<DWORD> Scanner::scan(const HANDLE& handle, const char* pattern, const char* mask, int length) {
+	MEMORY_BASIC_INFORMATION mbi;
+	SIZE_T lpNumberOfBytesRead;
+	char* buffer;
+	std::vector<DWORD> result;
+
+	for (SIZE_T base_addr = 0x0; VirtualQueryEx(handle, (LPCVOID)base_addr, &mbi, sizeof(MEMORY_BASIC_INFORMATION)); base_addr += mbi.RegionSize) {
+		if (mbi.Protect != PAGE_READWRITE || mbi.Type != MEM_PRIVATE) continue;
+		buffer = new char[mbi.RegionSize];
+		ReadProcessMemory(handle, mbi.BaseAddress, buffer, mbi.RegionSize, &lpNumberOfBytesRead);
+		auto scan_result = find_pattern(buffer, lpNumberOfBytesRead, pattern, length, mask);
+		if (scan_result.size() != 0) {
+			for (const auto& e : scan_result) {
+				result.push_back(e + base_addr);
+			}
+		}
+		delete[] buffer;
 	}
 	return result;
 }
