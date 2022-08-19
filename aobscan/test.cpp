2397907fd26a4417334cb2c1a8bf5da114d5cdff
riari <chanhu0902@gmail.com>
2022-08-19 22:04:33 +0900

    add: kmp검색에 mask 지원

--- aobscan/test.cpp
+++ aobscan/test.cpp
@@ -5,7 +5,7 @@ int main() {
 	std::vector<DWORD> result;
 	HANDLE handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, 0x7B78);
 	clock_t start = clock();
-	result = Scanner::scan(handle, "10 10 10 10 10 10 10 20 20");
+	result = Scanner::scan(handle, "10 10 ?? ?? ?? 20 20");
 	clock_t end = clock();
 	std::cout << std::endl << (end - start) << std::endl << result.size();
 }
