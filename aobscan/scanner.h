f4c8ad16470006c23539755e130202f516be399b
riari <chanhu0902@gmail.com>
2022-08-19 16:04:15 +0900

    fix: 함수 원형  재정의

--- aobscan/scanner.h
+++ aobscan/scanner.h
@@ -9,8 +9,7 @@ class Scanner {
 public:
 	static std::vector<DWORD> scan(const HANDLE& handle, const std::string& pattern);
 private:
-	static std::vector<DWORD> scan(const HANDLE& handle, const char* pattern, const char* mask);
-	static std::vector<DWORD> scan(const HANDLE& handle, const char* pattern);
+	static std::vector<DWORD> scan(const HANDLE& handle, const char* pattern, const char* mask, int length);
 	static std::vector<DWORD> find_pattern(const char* buffer, int buffer_length, const char* pattern, int pattern_length, const char* mask);
 };
 
